0e938a65004b6f3efee4d2ed5d60475aa06cdf00
Pavel Spakowski <Pavel.Spakowski@icloud.com>
2024-12-02 21:23:35 +0100

    feat(day 1): Completed the solution

--- advent-of-code-2024/src/day-1/src/main.rs
+++ advent-of-code-2024/src/day-1/src/main.rs
@@ -1,5 +1,58 @@
-mod input;
+use std::fs;
+const FILE_PATH: &str = "input.txt";
 
 fn main() {
-    println!("{}", input::RAW_INPUT);
+    let mut content = fs::read_to_string(FILE_PATH)
+        .expect("Failed to read file");
+
+    // Check for and remove the BOM
+    if content.starts_with('\u{feff}') {
+        content = content.trim_start_matches('\u{feff}').to_string();
+    }
+
+    let string_entries: Vec<&str> = content.split_whitespace().collect();
+
+    let int_entries: Vec<i32> = string_entries
+        .iter()
+        .map(|s| s.parse::<i32>().expect("Failed to parse string as integer"))
+        .collect();
+
+    let mut left: Vec<_> = int_entries
+        .iter()
+        .enumerate()
+        .filter(|&(index, _)| index % 2 == 0)
+        .map(|(_, &value)| value)
+        .collect();
+
+    left.sort();
+
+    let mut right: Vec<_> = int_entries
+        .iter()
+        .enumerate()
+        .filter(|&(index, _)| index % 2 != 0)
+        .map(|(_, &value)| value)
+        .collect();
+
+    right.sort();
+
+    let distances: Vec<i32> = left
+        .iter()
+        .zip(&right)
+        .map(|(a, b)| (b - a).abs())
+        .collect();
+
+    let all: Vec<String> = left
+        .iter()
+        .zip(&right)
+        .zip(distances.iter())
+        .map(|((a, b), d)| format!("left: {}, right : {}, distance: {}", a, b, d))
+        .collect();
+
+    let distance: i32 = distances.iter().sum();
+
+//    for item in &all {
+//        println!("{}", item);
+//    }
+
+    println!("Day 1 answer is {}", distance);
 }
