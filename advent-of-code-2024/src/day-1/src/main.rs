af8c0dc0eba7e837493331aef10981f30f234333
Pavel Spakowski <Pavel.Spakowski@iav.de>
2024-12-03 09:11:34 +0100

    feat(day 1): Separated logic into functions

--- advent-of-code-2024/src/day-1/src/main.rs
+++ advent-of-code-2024/src/day-1/src/main.rs
@@ -2,7 +2,23 @@ use std::fs;
 const FILE_PATH: &str = "input.txt";
 
 fn main() {
-    let mut content = fs::read_to_string(FILE_PATH)
+
+    let file_content: String = get_file_contents(FILE_PATH);
+
+    let int_entries: Vec<i32> = get_numbers_from_file_content(&file_content);
+
+    let left: Vec<i32> = get_sorted_vector(int_entries.clone(), false);
+    let right: Vec<i32> = get_sorted_vector(int_entries.clone(), true);
+
+    let distances: Vec<i32> = calculate_distances(left, right);
+
+    let distance: i32 = sum_distances(distances);
+
+    println!("Day 1 answer is {}", distance);
+}
+
+fn get_file_contents(filename: &str) -> String {
+    let mut content = fs::read_to_string(filename)
         .expect("Failed to read file");
 
     // Check for and remove the BOM
@@ -10,49 +26,43 @@ fn main() {
         content = content.trim_start_matches('\u{feff}').to_string();
     }
 
-    let string_entries: Vec<&str> = content.split_whitespace().collect();
+    content
+}
+
+fn get_numbers_from_file_content(file_content: &str) -> Vec<i32> {
+    let string_entries: Vec<&str> = file_content.split_whitespace().collect();
 
     let int_entries: Vec<i32> = string_entries
         .iter()
         .map(|s| s.parse::<i32>().expect("Failed to parse string as integer"))
         .collect();
 
-    let mut left: Vec<_> = int_entries
+    int_entries
+}
+
+fn get_sorted_vector(vec: Vec<i32>, odd: bool) -> Vec<i32> {
+    let mut result: Vec<_> = vec
         .iter()
         .enumerate()
-        .filter(|&(index, _)| index % 2 == 0)
+        .filter(|&(index, _)| index % 2 == if odd { 0 } else { 1 })
         .map(|(_, &value)| value)
         .collect();
 
-    left.sort();
+    result.sort();
 
-    let mut right: Vec<_> = int_entries
-        .iter()
-        .enumerate()
-        .filter(|&(index, _)| index % 2 != 0)
-        .map(|(_, &value)| value)
-        .collect();
+    result
+}
 
-    right.sort();
-
+fn calculate_distances(left: Vec<i32>, right: Vec<i32>) -> Vec<i32> {
     let distances: Vec<i32> = left
         .iter()
         .zip(&right)
         .map(|(a, b)| (b - a).abs())
         .collect();
 
-    let all: Vec<String> = left
-        .iter()
-        .zip(&right)
-        .zip(distances.iter())
-        .map(|((a, b), d)| format!("left: {}, right : {}, distance: {}", a, b, d))
-        .collect();
+    distances
+}
 
-    let distance: i32 = distances.iter().sum();
-
-//    for item in &all {
-//        println!("{}", item);
-//    }
-
-    println!("Day 1 answer is {}", distance);
+fn sum_distances(distances: Vec<i32>) -> i32 {
+    distances.iter().sum()
 }
