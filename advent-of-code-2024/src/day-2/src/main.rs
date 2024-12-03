bc011403a287d95e1e36c66ee665c2bb6c8c163a
Pavel Spakowski <Pavel.Spakowski@icloud.com>
2024-12-03 18:16:09 +0100

    feat(day 2): Completed the solution

--- advent-of-code-2024/src/day-2/src/main.rs
+++ advent-of-code-2024/src/day-2/src/main.rs
@@ -1,6 +1,6 @@
 use utils::{convert_string_vector_to_integer_vector, get_file_contents};
 
-const FILE_PATH: &str = "input_mock.txt";
+const FILE_PATH: &str = "input.txt";
 
 fn main() {
 
@@ -9,8 +9,16 @@ fn main() {
     let lines: Vec<&str> = get_lines_from_file_content(&file_content);
 
     let report_vectors = get_report_integer_vectors(get_report_string_vectors(lines));
-
-    println!("{:?}", report_vectors);
+    
+    let mut counter = 0;
+    
+    for item in &report_vectors {
+        if is_vector_safe(&item) {  
+            counter += 1;
+        }
+    }
+    
+    println!("Day 2 answer is {}", counter);
 }
 
 fn get_lines_from_file_content(file_content: &str) -> Vec<&str> {
@@ -23,4 +31,20 @@ fn get_report_string_vectors(lines: Vec<&str>) -> Vec<Vec<&str>> {
 
 fn get_report_integer_vectors(string_vectors: Vec<Vec<&str>>) -> Vec<Vec<i32>> {
     string_vectors.into_iter().map(|string_vector| convert_string_vector_to_integer_vector(string_vector)).collect()
+}
+
+fn is_decreasing(vector: &Vec<i32>) -> bool {
+    vector.windows(2).all(|pair| pair[0] > pair[1])
+}
+
+fn is_increasing(vector: &Vec<i32>) -> bool {
+    vector.windows(2).all(|pair| pair[0] < pair[1])
+}
+
+fn is_step_valid(vector: &Vec<i32>) -> bool {
+    vector.windows(2).all(|pair| (pair[0] - pair[1]).abs() <= 3)
+}
+
+fn is_vector_safe(vector: &Vec<i32>) -> bool {
+    (is_increasing(vector) || is_decreasing(vector)) && is_step_valid(vector)
 }
