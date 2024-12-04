9bac775b6b55fef5a33106b4ef4df477bd3ab85b
Pavel Spakowski <Pavel.Spakowski@icloud.com>
2024-12-04 21:59:14 +0100

    feat(day 1): Resolved day 1 part two

--- advent-of-code-2024/src/day-1/src/main.rs
+++ advent-of-code-2024/src/day-1/src/main.rs
@@ -11,11 +11,15 @@ fn main() {
     let left: Vec<i32> = get_sorted_vector(int_entries.clone(), false);
     let right: Vec<i32> = get_sorted_vector(int_entries.clone(), true);
 
-    let distances: Vec<i32> = calculate_distances(left, right);
+    let distances: Vec<i32> = calculate_distances(&left, &right);
 
     let distance: i32 = sum_distances(distances);
 
-    println!("Day 1 answer is {}", distance);
+    println!("Day 1 Part One answer is {}", distance);
+    
+    let score = calculate_similarity_score(&left, &right);
+    
+    println!("Day 1 Part Two answer is {}", score);
 }
 
 fn get_numbers_from_file_content(file_content: &str) -> Vec<i32> {
@@ -37,10 +41,10 @@ fn get_sorted_vector(vec: Vec<i32>, odd: bool) -> Vec<i32> {
     result
 }
 
-fn calculate_distances(left: Vec<i32>, right: Vec<i32>) -> Vec<i32> {
+fn calculate_distances(left: &Vec<i32>, right: &Vec<i32>) -> Vec<i32> {
     let distances: Vec<i32> = left
         .iter()
-        .zip(&right)
+        .zip(right)
         .map(|(a, b)| (b - a).abs())
         .collect();
 
@@ -49,4 +53,18 @@ fn calculate_distances(left: Vec<i32>, right: Vec<i32>) -> Vec<i32> {
 
 fn sum_distances(distances: Vec<i32>) -> i32 {
     distances.iter().sum()
+}
+
+fn calculate_similarity_score(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
+    let mut score: i32 = 0;
+    
+    for item in left {
+        score += item * get_count_by_number(right, *item);
+    }
+    
+    score
+}
+
+fn get_count_by_number(vector: &Vec<i32>, target: i32) -> i32 {
+    vector.iter().filter(|&&x| x == target).count() as i32
 }
