feea8940a74596a5e1aa16bb5af6f6564e6943e5
Pavel Spakowski <Pavel.Spakowski@icloud.com>
2024-12-06 17:06:40 +0100

    feat(day 6): Unfinished day-6 part one

--- advent-of-code-2024/src/day-6/src/main.rs
+++ advent-of-code-2024/src/day-6/src/main.rs
@@ -1,8 +1,112 @@
-use utils::{get_file_contents};
+use utils::{get_file_contents, get_grid};
 
-const FILE_PATH: &str = "input";
+const FILE_PATH: &str = "input_mock";
 
 fn main() {
     let file_content: String = get_file_contents(FILE_PATH);
-    println!("Hello, world!");
+    let mut grid: Vec<Vec<char>> = get_grid(&file_content);
+
+    println!("Got grid {}x{}", grid.len(), grid[0].len());
+
+    if let Some((start_row, start_col)) = get_start_pos(&grid) {
+        println!(
+            "Got start position row = {}, col = {}",
+            start_row, start_col
+        );
+        println!("Day 3 Part One answer is {}", walk(&mut grid, (start_row, start_col)));
+    } else {
+        println!("No start position found!");
+    };
+
+}
+
+fn get_start_pos(grid: &Vec<Vec<char>>) -> Option<(usize, usize)> {
+    for (row_index, row) in grid.iter().enumerate() {
+        for (col_index, &ch) in row.iter().enumerate() {
+            if ch == '^' {
+                return Some((row_index, col_index));
+            }
+        }
+    }
+    None
+}
+
+fn walk(grid: &mut Vec<Vec<char>>, start_pos: (usize, usize)) -> i32{
+    let rows = grid.len();
+    let cols = grid[0].len();
+
+    let mut position = start_pos;
+    let mut steps = 0;
+
+    loop {
+        let (row, col) = position;
+        let next_row: isize;
+        let next_col: isize;
+        println!("Position {}:{} (steps {})", row, col, steps);
+
+        // Check bounds
+        if row >= rows || col >= cols {
+            break
+        }
+        
+        grid[row][col] = 'X';
+        print_grid(&grid);
+
+        if is_an_obstacle_next(&grid, position) {
+            //grid[row][col] = 'O';
+            println!("Turned at {}:{}", position.0, position.1);
+            rotate_90_counterclockwise(grid);
+            next_row = (cols - col) as isize - 2;
+            next_col = row as isize;  // 1:4 -> 5:1
+        } else {
+            next_row = row as isize - 1;
+            next_col = col as isize;
+        }
+        //grid[row][col] = 'X';
+        
+        // Break if we go out of bounds
+        if next_row < 0 || next_col < 0 || next_row >= rows as isize || next_col >= cols as isize {
+            break;
+        }
+
+        position = (next_row as usize, next_col as usize);
+        println!("Next step into {}:{}", next_row, next_col);
+        steps += 1;
+        
+        // debug
+        if steps > 150 {
+            break;
+        }
+        
+        //print_grid(&grid);
+    }
+    
+    steps
+}
+
+fn is_an_obstacle_next(grid: &Vec<Vec<char>>, pos: (usize, usize)) -> bool {
+    if pos.0 == 0 {
+        return false;
+    }
+
+    grid[pos.0 - 1][pos.1] != '.' && grid[pos.0 - 1][pos.1] != 'X' && grid[pos.0 + 1][pos.1] != 'O'
+}
+
+fn rotate_90_counterclockwise(grid: &mut Vec<Vec<char>>) {
+    let rows = grid.len();
+    let cols = grid[0].len();
+    //let mut rotated = vec![vec![' '; rows]; cols];
+    let rotated = grid.clone();
+
+    for row in 0..rows {
+        for col in 0..cols {
+            grid[cols - col - 1][row] = rotated[row][col];
+        }
+    }
+}
+
+fn print_grid(grid: &Vec<Vec<char>>) {
+    for row in grid.iter() {
+        println!("{:?}", row);
+    }
 }
