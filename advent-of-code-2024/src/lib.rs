13a33ecee0421dddbe33e0d7498c922eb93f3dce
Pavel Spakowski <Pavel.Spakowski@icloud.com>
2024-12-06 14:06:43 +0100

    feat(day 6): Initiate day-6

--- advent-of-code-2024/src/lib.rs
+++ advent-of-code-2024/src/lib.rs
@@ -1,8 +1,7 @@
-﻿use std::fs;
+use std::fs;
 
 pub fn get_file_contents(filename: &str) -> String {
-    let mut content = fs::read_to_string(filename)
-        .expect("Failed to read file");
+    let mut content = fs::read_to_string(filename).expect("Failed to read file");
 
     // Check for and remove the BOM
     if content.starts_with('\u{feff}') {
