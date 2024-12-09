eb0d5d6672798494708650ce23c3fa72a3547a2a
Pavel Spakowski <Pavel.Spakowski@icloud.com>
2024-12-09 01:27:39 +0100

    feat(day 8): Resolved part two

--- advent-of-code-2024/day-8/src/main.rs
+++ advent-of-code-2024/day-8/src/main.rs
@@ -1,10 +1,9 @@
 use std::time::Instant;
-use utils::{get_file_contents, get_grid, print_grid};
+use utils::{get_file_contents, get_grid};
 
 const FILE_PATH: &str = "input";
 
-#[derive(Debug, Copy, Clone)]
-#[derive(PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 struct Point {
     row: usize,
     col: usize,
@@ -14,9 +13,10 @@ fn main() {
     let start_time = Instant::now();
     let file_content: String = get_file_contents(FILE_PATH);
 
-    let mut grid: Vec<Vec<char>> = get_grid(&file_content);
+    let grid: Vec<Vec<char>> = get_grid(&file_content);
     let grid_size: (usize, usize) = (grid.len(), grid[0].len());
-    let mut antinods: Vec<Point> = Vec::new();
+    let mut antinods_part1: Vec<Point> = Vec::new();
+    let mut antinods_part2: Vec<Point> = Vec::new();
 
     // main loop
     for row in 0..grid.len() {
@@ -35,36 +35,48 @@ fn main() {
                         continue;
                     }
                     if grid[row_inner][col_inner] == grid[row][col] {
-                        let valid_targets: Vec<Point> = extrapolate_line(
+                        get_points_part1(
                             Point { row, col },
                             Point {
                                 row: row_inner,
                                 col: col_inner,
                             },
                             grid_size,
-                        );
-
-                        valid_targets.iter().for_each(|p| {
-                            // if grid[p.row][p.col] == '.' || grid[p.row][p.col] == '#' {
-                            //     grid[p.row][p.col] = '#';
-                            //     }
-                            if !antinods.contains(&p) {
-                                antinods.push(*p);
+                        )
+                        .iter()
+                        .for_each(|p| {
+                            if !antinods_part1.contains(&p) {
+                                antinods_part1.push(*p);
                             }
                         });
+
+                        get_points_part2(
+                            Point { row, col },
+                            Point {
+                                row: row_inner,
+                                col: col_inner,
+                            },
+                            grid_size,
+                        )
+                        .iter()
+                        .for_each(|p| {
+                            if !antinods_part2.contains(&p) {
+                                antinods_part2.push(*p);
+                            }
+                        })
                     }
                 }
             }
         }
     }
-    
-    println!("Day 8 Part One answer is {}", antinods.len());
-    println!("Day 8 Part Two answer is {}", 0);
+
+    println!("Day 8 Part One answer is {}", antinods_part1.len());
+    println!("Day 8 Part Two answer is {}", antinods_part2.len());
 
     println!("Execution time: {:.2?}", start_time.elapsed());
 }
 
-fn extrapolate_line(p1: Point, p2: Point, grid_size: (usize, usize)) -> Vec<Point> {
+fn get_points_part1(p1: Point, p2: Point, grid_size: (usize, usize)) -> Vec<Point> {
     let mut result: Vec<Point> = Vec::new();
     let d_row: isize = p2.row as isize - p1.row as isize;
     let d_col: isize = p2.col as isize - p1.col as isize;
@@ -73,20 +85,55 @@ fn extrapolate_line(p1: Point, p2: Point, grid_size: (usize, usize)) -> Vec<Poin
     let p2_row: isize = p2.row as isize + d_row;
     let p2_col: isize = p2.col as isize + d_col;
 
-    if !is_out_of_bounce(grid_size, (p1_row, p1_col)) {
-        result.push(Point {
-            row: p1_row as usize,
-            col: p1_col as usize,
-        })
-    }
-    if !is_out_of_bounce(grid_size, (p2_row, p2_col)) {
-        result.push(Point {
-            row: p2_row as usize,
-            col: p2_col as usize,
-        })
-    }
+    point_loop_part1(grid_size, &mut result, p1_row, p1_col);
+    point_loop_part1(grid_size, &mut result, p2_row, p2_col);
 
     result
+}
+
+fn point_loop_part1(grid_size: (usize, usize), result: &mut Vec<Point>, row: isize, col: isize) {
+    if !is_out_of_bounce(grid_size, (row, col)) {
+        result.push(Point {
+            row: row as usize,
+            col: col as usize,
+        })
+    }
+}
+
+fn get_points_part2(p1: Point, p2: Point, grid_size: (usize, usize)) -> Vec<Point> {
+    let mut result: Vec<Point> = Vec::new();
+    let d_row: isize = p2.row as isize - p1.row as isize;
+    let d_col: isize = p2.col as isize - p1.col as isize;
+
+    point_loop_part2(p1, grid_size, &mut result, d_row, d_col);
+    point_loop_part2(p2, grid_size, &mut result, d_row, d_col);
+
+    result
+}
+
+fn point_loop_part2(
+    point: Point,
+    grid_size: (usize, usize),
+    result: &mut Vec<Point>,
+    d_row: isize,
+    d_col: isize,
+) {
+    let mut step: isize = 1;
+
+    while true {
+        let row: isize = point.row as isize - d_row * step;
+        let col: isize = point.col as isize - d_col * step;
+
+        if !is_out_of_bounce(grid_size, (row, col)) {
+            result.push(Point {
+                row: row as usize,
+                col: col as usize,
+            })
+        } else {
+            break;
+        }
+        step += 1;
+    }
 }
 
 fn is_out_of_bounce(grid_size: (usize, usize), point: (isize, isize)) -> bool {
